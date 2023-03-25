ab72b8375f8c20ff3ddef10a1ee4dab3c6380743
iamalmamun <abdullahalmamun.dev@gmail.com>
2023-03-25 13:26:03 +0600

    bank-website full added

---
+++ js/withdraw.js
@@ -0,0 +1,27 @@
+// step:1
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // step:2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    // step:3
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    
+    // step:4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+    // step:5
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step:6
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotalElement.innerText = currentBalanceTotal;
+    // step:7
+    withdrawField.value = '';
+})
